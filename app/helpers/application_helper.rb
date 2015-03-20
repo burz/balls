64da8b4094780272ddcf40bbe985ecb89f527e9d
Anthony Burzillo <burz@jhu.edu>
2015-03-20 11:57:17 -0400

    Add colors to the ratio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_ratio ratio
+    if ratio >= 0
+      ('<font color="green">+' + ratio.to_s + '</font>').html_safe
+    else
+      ('<font color="red">' + ratio.to_s + '</font>').html_safe
+    end
+  end
 end
