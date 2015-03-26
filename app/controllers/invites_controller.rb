01e5cfb0208beb754fb9531eb8839de39751ca6e
Anthony Burzillo <burz@jhu.edu>
2015-03-26 17:15:21 -0400

    wowowowowowowowowo

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,5 +1,5 @@
 class InvitesController < ApplicationController
-  require_relative '../../mixins/constant.rb'
+  require_relative '../../mixins/constants'
 
   before_action :authenticate_user!
   before_action :set_leagues
