601545f55b7af2b6ed2cc2f417922e24bfbd9e19
Anthony Burzillo <burz@jhu.edu>
2015-03-26 17:13:43 -0400

    Add extension

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,5 +1,5 @@
 class InvitesController < ApplicationController
-  require_relative '../../mixins/constants'
+  require_relative '../../mixins/constant.rb'
 
   before_action :authenticate_user!
   before_action :set_leagues
