9c4fb0e57bb1f0a7438ec7f86cba4cd7a88514ef
Anthony Burzillo <burz@jhu.edu>
2015-03-28 16:49:26 -0400

    Preload email in signup from invites

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,8 +1,8 @@
 class InvitesController < ApplicationController
   include Constants
 
-  before_action :authenticate_user!
-  before_action :set_leagues
+  before_action :authenticate_user!, except: [:join]
+  before_action :set_leagues, except: [:join]
 
   def set_leagues
     @leagues = current_user.leagues
@@ -20,29 +20,41 @@ class InvitesController < ApplicationController
     render nothing: true
   end
 
-  def join
-    invite = Invite.where(token: params[:token]).first
-    league = invite.league
-    if invite.nil?
-      flash[:notice] = 'The token has expired'
-      redirect_to controller: :welcome, action: :index
+  def forward_user?
+    if not user_signed_in?
+      session['user_return_to'] = request.fullpath
+      redirect_to new_user_session_path(email: params[:email])
+      true
     else
-      if league.users.where(id: current_user).first != nil
-        flash[:notice] = 'You are already a member of "' + league.name + '"'
-        Invite.destroy invite
-        redirect_to controller: :leagues, action: :show, id: league
+      false
+    end
+  end
+
+  def join
+    if not forward_user?
+      invite = Invite.where(token: params[:token]).first
+      league = invite.league
+      if invite.nil?
+        flash[:notice] = 'The token has expired'
+        redirect_to controller: :welcome, action: :index
       else
-        LeagueMembership.create user: current_user, league: league, admin: false
-        LeagueRating.create user: current_user, league: league,
-                            rating: LEAGUE_START,
-                            games_played: 0, wins: 0, losses: 0
-        Invite.destroy invite
-        league.seasons.each do |season|
-          SeasonRating.create user: current_user, season: season,
-                              rating: SEASON_START,
+        if league.users.where(id: current_user).first != nil
+          flash[:notice] = 'You are already a member of "' + league.name + '"'
+          Invite.destroy invite
+          redirect_to controller: :leagues, action: :show, id: league
+        else
+          LeagueMembership.create user: current_user, league: league, admin: false
+          LeagueRating.create user: current_user, league: league,
+                              rating: LEAGUE_START,
                               games_played: 0, wins: 0, losses: 0
+          Invite.destroy invite
+          league.seasons.each do |season|
+            SeasonRating.create user: current_user, season: season,
+                                rating: SEASON_START,
+                                games_played: 0, wins: 0, losses: 0
+          end
+          redirect_to controller: :leagues, action: :show, id: league
         end
-        redirect_to controller: :leagues, action: :show, id: league
       end
     end
   end
