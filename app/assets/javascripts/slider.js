26357832af796c05255d381b9427aa3eb7d15f1c
Anthony Burzillo <burz@jhu.edu>
2015-03-30 06:17:37 -0400

    Create callbacks for slideouts

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,22 +1,22 @@
 function slider_ready () {
   var slideout = new Slideout({
-    'panel': $('#panel')[0],
-    'menu': $('#menu')[0],
-    'padding': 256,
-    'tolerance': 70
+    panel: $('#panel')[0],
+    menu: $('#menu')[0],
+    padding: 256,
+    tolerance: 70
   });
   $('#menu_button').click(function () {
     slideout.toggle();
-    if(!slideout.isOpen()) {
-      setTimeout(function () {
-        $('#panel').attr('style', '');
-      }, 250);
-    }
   });
   $('.menu').click(function (event_object) {
     var node_name = event_object.target.nodeName;
     if(node_name === 'A' || node_name === 'HEADER' || node_name === 'SPAN') {
       slideout.close();
+    }
+  });
+  slideout.change(function () {
+    if(!slideout.isOpen()) {
+      $('#panel').attr('style', '');
     }
   });
 }
