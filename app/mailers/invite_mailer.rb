d2c07ceffcc826e91885731a68d2e74e03da662c
Anthony Burzillo <burz@jhu.edu>
2015-04-07 21:29:33 -0400

    Update invite mailer

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,11 +2,12 @@ class InviteMailer < ApplicationMailer
   default from: 'invites@ballsapp.com'
 
   def invite_email user, email, league, token
+    @user = user
     @email = email
     @league = league
     @token = token
-    @subject = user.name + ' (' + user.email +
-               ') has invited you to "' + league.name + '"'
-    mail to: email, subject: @subject
+    subject = user.name + ' has invited you to join the ' +
+              league.name + ' beer pong league'
+    mail to: email, subject: subject
   end
 end
