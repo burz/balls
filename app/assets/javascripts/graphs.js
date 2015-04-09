5fca10f9f295a75c713b81c8de6dd4c8a91829d6
Anthony Burzillo <burz@jhu.edu>
2015-04-09 19:26:55 -0400

    Fix loading of graphs

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,10 +1,16 @@
-function graph_ready () {
+var league_ratings_graph = null;
+var season_ratings_graph = null;
+function graphs_ready () {
   var margin = { top: 10, bottom: 80, left: 65, right: 20 };
   var width = $('#graphs_container').width();
   var height = 450;
-  var league_ratings_graph = new TimeGraph(margin, width, height, '#league_graph_canvas');
+  league_ratings_graph = new TimeGraph(margin, width, height, '#league_graph_canvas');
   league_ratings_graph.loadGraph(league_ratings_data);
-  var season_ratings_graph = new TimeGraph(margin, width, height, '#season_graph_canvas');
+  season_ratings_graph = new TimeGraph(margin, width, height, '#season_graph_canvas');
   season_ratings_graph.loadGraph(season_ratings_data);
   $('#league_ratings_graph').hide();
+}
+function reload_graphs () {
+  league_ratings_graph.reloadGraph(league_ratings_data);
+  season_ratings_graph.reloadGraph(season_ratings_data);
 }
