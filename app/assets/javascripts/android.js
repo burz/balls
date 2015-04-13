a8aa7e063950d4788d0ece3430adb54a83f94fc8
Anthony Burzillo <burz@jhu.edu>
2015-04-13 06:22:30 -0400

    Update contacts workflow on android

--- app/assets/javascripts/android.js
+++ app/assets/javascripts/android.js
@@ -1,6 +1,17 @@
-function send_invite_android(number) {
-  $.get($('#invite_contacts').attr('league_generate_invite_path'), function (data) {
-    BallsAppAndroid.sendInvite(number, data.league_name, data.invite_path);
+function send_contact_invites_android () {
+  start_spinner();
+  var contacts = $('#contact_list').children();
+  var count = contacts.length;
+  var send_invite_android = function (number) {
+    $.get($('#invite_contacts').attr('league_generate_invite_path'), function (data) {
+      BallsAppAndroid.sendInvite(number, data.league_name, data.invite_path);
+      if(--count === 0) {
+        Turbolinks.visit($('#invite_contacts').attr('league_path'));
+      }
+    });
+  };
+  $('#contact_list').children().each(function (i, contact) {
+    send_invite_android(contact.getAttribute('phone_number'));
   });
 }
 function load_contacts_android () {
@@ -8,9 +19,5 @@ function load_contacts_android () {
 }
 function android_ready () {
   load_contacts_android();
-  $('#send_contact_invites').click(function () {
-    $('#contact_list').children().each(function (i, contact) {
-      send_invite_android(contact.getAttribute('phone_number'));
-    });
-  });
+  $('#send_contact_invites').click(send_contact_invites_android);
 }
