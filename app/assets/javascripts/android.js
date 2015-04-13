8f5e0f771dfa061e524a5d8968f0ed32069e4474
Anthony Burzillo <burz@jhu.edu>
2015-04-13 05:10:23 -0400

    Hook up android functionality

--- app/assets/javascripts/android.js
+++ app/assets/javascripts/android.js
@@ -1,10 +1,16 @@
-function send_contact_invite_android(number) {
+function send_invite_android(number) {
   $.get($('#invite_contacts').attr('league_generate_invite_path'), function (data) {
     BallsAppAndroid.sendInvite(number, data.league_name, data.invite_path);
   });
 }
+function load_contacts_android () {
+  BallsAppAndroid.loadContacts();
+}
 function android_ready () {
+  load_contacts_android();
   $('#send_contact_invites').click(function () {
-    
+    $('#contact_list').children().each(function (i, contact) {
+      send_invite_android(contact.getAttribute('phone_number'));
+    });
   });
 }
